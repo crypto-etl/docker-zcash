8b65086402d85e919a7cff3896851bd4f10c1368
W-Mark Kubacki <wmark@hurrikane.de>
2016-11-21 20:59:52 +0100

    -Wl,--gc-sections

--- zcash/runlevels/builder/10.prepare.sh
+++ zcash/runlevels/builder/10.prepare.sh
@@ -13,8 +13,8 @@ apt-get -y install \
 
 # Buildflags, which will influence the build as environment variables.
 cat >/etc/dpkg/buildflags.conf <<EOF
-SET CFLAGS   -Ofast -march=silvermont -mtune=broadwell -fipa-cp -fipa-cp-alignment -ftree-vectorize -ftree-loop-if-convert -fschedule-insns -fsched-pressure -maes -mcx16 -mfpmath=387+sse -mfxsr -mmmx -mpclmul -mpopcnt -msahf -msse4 -mno-movbe -fstack-protector-strong -fpie -fpic
-SET CXXFLAGS -Ofast -march=silvermont -mtune=broadwell -fipa-cp -fipa-cp-alignment -ftree-vectorize -ftree-loop-if-convert -fschedule-insns -fsched-pressure -maes -mcx16 -mfpmath=387+sse -mfxsr -mmmx -mpclmul -mpopcnt -msahf -msse4 -mno-movbe -fstack-protector-strong -fpie -fpic
+SET CFLAGS   -Ofast -march=silvermont -mtune=broadwell -fipa-cp -fipa-cp-alignment -ftree-vectorize -ftree-loop-if-convert -fschedule-insns -fsched-pressure -maes -mcx16 -mfpmath=387+sse -mfxsr -mmmx -mpclmul -mpopcnt -msahf -msse4 -mno-movbe -fstack-protector-strong -fpie -fpic -ffunction-sections -fdata-sections
+SET CXXFLAGS -Ofast -march=silvermont -mtune=broadwell -fipa-cp -fipa-cp-alignment -ftree-vectorize -ftree-loop-if-convert -fschedule-insns -fsched-pressure -maes -mcx16 -mfpmath=387+sse -mfxsr -mmmx -mpclmul -mpopcnt -msahf -msse4 -mno-movbe -fstack-protector-strong -fpie -fpic -ffunction-sections -fdata-sections
 SET CPPFLAGS -Wp,-D_FORTIFY_SOURCE=2
-SET LDFLAGS  -Wl,-O1 -Wl,-z,relro -Wl,-z,now -Wl,-z,nodynamic-undefined-weak -Wl,--as-needed
+SET LDFLAGS  -Wl,-O1 -Wl,-z,relro -Wl,-z,now -Wl,-z,nodynamic-undefined-weak -Wl,--as-needed -Wl,--gc-sections -fwhole-program
 EOF
